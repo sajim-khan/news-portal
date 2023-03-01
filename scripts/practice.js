3324573121d4704b1af38909cd75510f82610e89
sajim-khan <shossain1427@gmail.com>
2023-03-01 18:14:31 +0600

    done upto alart section

--- scripts/practice.js
+++ scripts/practice.js
@@ -26,10 +26,12 @@ const fetchCategoryNews = (category_id, category_name) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) =>showAllNews(data, category_name)
+    .then((data) =>showAllNews(data.data, category_name)
     );
 };
 
 const showAllNews = (data, category_name) => {
     console.log(data, category_name);
+    document.getElementById('news-count').innerText = data.length;
+    document.getElementById('category-name').innerText = category_name;
 }
