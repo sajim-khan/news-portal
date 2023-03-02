8e256b66c5901a842c47daaaebfd2c660920a548
sajim-khan <shossain1427@gmail.com>
2023-03-02 17:09:58 +0600

    added news details fetch

--- scripts/practice.js
+++ scripts/practice.js
@@ -35,14 +35,60 @@ const showAllNews = (data, category_name) => {
     document.getElementById('news-count').innerText = data.length;
     document.getElementById('category-name').innerText = category_name;
     
-    data.forEach(singleNews => {
-        console.log(singleNews);
-    })
-    
-    const newsContainer = document.getElementById("all-news");
-  newsContainer.innerHTML = "";
+  const newsContainer = document.getElementById("all-news");
+  newsContainer.innerHTML = '';
   data.forEach((singleNews) => {
-    const { _id, image_url, title, details, author, total_view,rating } = singleNews;
-    
-  })
+  //console.log(singleNews);
+  //destructuring
+  const { _id, image_url, title, details, author, total_view,rating } = singleNews;
+  //newsContainer.innerHTML += ``
+  // For card showing
+  const card = document.createElement("div");
+  card.classList.add('card', 'mb-3');
+  card.innerHTML = `
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${image_url
+        }" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${details.slice(0, 200)}...</p>
+        </div>
+        
+        <div class="card-footer border-0 bg-body d-flex justify-content-between">
+          <div class="d-flex gap-2">
+            <div>
+            <img src="${author.img}" class="img-fluid rounded-circle mt-2" alt="" height="30" width="40">
+            </div>
+            <div>
+              <p class="m-0 p-0">${author.name}</p>
+              <p class="m-0 p-0">${author.published_date}</p>
+            </div>  
+          </div>
+          <div class="d-flex align-items-center">
+            <i class="fas fa-eye"></i>
+            <p class="m-0 p-0">${total_view}</p>
+          </div>
+          <div>
+            <i class="fas fa-star"></i>
+          </div>
+          <div>
+            <i class="fas fa-arrow-right" onclick="showNewsDetail('${_id}')"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+  newsContainer.appendChild(card)
+})
+};
+
+// After Arrow buttons click it shows details news
+
+const showNewsDetail = news_id => {
+  let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+  console.log(url);
 }
+
